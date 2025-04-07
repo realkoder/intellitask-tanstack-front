19d3bc23a6c8636b2ee851102923ffc39d03e96b
realkoder <alexanderbtcc@gmail.com>
2025-04-07 18:23:46 +0200

    added import alias

--- app.config.ts
+++ app.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
     resolve: {
       alias: {
         '~encore': path.resolve(__dirname, './encore.gen'),
+        '@': path.resolve(__dirname, 'src'), // Adjust this path if necessary
       },
     },
   },
