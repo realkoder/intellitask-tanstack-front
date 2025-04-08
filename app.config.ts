1d936d58a39fffdf7fe699e7275e2d3d24df473a
realkoder <alexanderbtcc@gmail.com>
2025-04-08 11:52:38 +0200

    added backend origin as allowedoriginst

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@tanstack/react-start/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import type { InlineConfig } from 'vite';
 import { fileURLToPath } from 'url';
 
 // Create dirname equivalent for ES modules
@@ -15,34 +16,38 @@ declare module '@tanstack/react-start/config' {
   }
 }
 
+const vite: InlineConfig = {
+  plugins: [
+    tailwindcss(),
+    tsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+  ],
+  server: {
+    allowedHosts: ["https://staging-intellitask-yrr2.encr.app"]
+  },
+  resolve: {
+    alias: {
+      '~encore': path.resolve(__dirname, './encore.gen'),
+    },
+  },
+  optimizeDeps: {
+    include: ['stream', 'stream/web', 'path', 'fs', 'async_hooks'], // Include Node.js built-in modules
+  },
+  build: {
+    rollupOptions: {
+      external: ['node:stream', 'node:stream/web', 'node:path', 'node:fs', 'node:async_hooks'], // Mark Node.js built-in modules as external
+    },
+  },
+}
+
 export default defineConfig({
   server: {
     _config: {
-
     }
   },
   tsr: {
     appDirectory: 'app',
   },
-  vite: {
-    plugins: [
-      tailwindcss(),
-      tsConfigPaths({
-        projects: ['./tsconfig.json'],
-      }),
-    ],
-    resolve: {
-      alias: {
-        '~encore': path.resolve(__dirname, './encore.gen'),
-      },
-    },
-    optimizeDeps: {
-      include: ['stream', 'stream/web', 'path', 'fs', 'async_hooks'], // Include Node.js built-in modules
-    },
-    build: {
-      rollupOptions: {
-        external: ['node:stream', 'node:stream/web', 'node:path', 'node:fs', 'node:async_hooks'], // Mark Node.js built-in modules as external
-      },
-    },
-  },
+  vite
 });
